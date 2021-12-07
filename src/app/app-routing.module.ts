290372cb5e8dc81de5901a20f4e95d88c9edcd3e
Williams <wilfre_padilla@hotmail.com>
2021-12-07 16:34:23 -0400

    feat: thanks component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { GratuityComponent } from './gratuity/gratuity.component';
 import { NotfoundComponent } from './shared/notfound/notfound.component';
+import { ThankyouComponent } from './shared/thankyou/thankyou.component';
 
 const routes: Routes = [
   { path: 'gratuity', component: GratuityComponent},
   { path: 'not-found', component: NotfoundComponent},
+  { path: 'thanks', component: ThankyouComponent},
   { path: '',   redirectTo: '/gratuity', pathMatch: 'full' },
   { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
